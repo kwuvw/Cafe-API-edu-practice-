4611e8c915ba4222c3a051ccab535579e1c3eef6
kwuvw <kostyak294@gmail.com>
2026-05-22 16:28:43 +0300

    api

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [UserController::class, 'login']);
 
-Route::prefix('api')->middleware('auth:sanctum')->group(function () {
+Route::prefix('api-cafe')->middleware('auth:sanctum')->group(function () {
     Route::get('/logout', [UserController::class, 'logout']);
 
     Route::middleware('role:admin')->group(function () {
